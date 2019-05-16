e5f06db6141d07d6d6f68b6ed9b43c064300ec9a
srydin <stevenrydin@gmail.com>
2019-05-16 16:56:51 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# zip-lookup
+# Zip Code Lookup
+
+A simple PHP utility for looking up city and/or state based on zip code. Takes a Zip argument request and returns an array with City and State info.
+
+## Getting Started
+
+The primary use case for this utility is form data handling, and removing the need for users to submit city/state info when Zip is sufficient.
+So wherever you're handling the form data, the included file contains a zip_lookup() function that will return an array with city and state data.
+```
+// Lookup city/state values
+$response = zip_lookup($zip);
+$city = $response['city'];
+$state = $response['state'];
+```
+
+Be sure to include the file in your form action page:
+```
+include_once('path-to-file/zip_lookup');
+```
+
+### Limitations
+
+This is not a small file. If motivated, you could convert the $data array to an SQL (or equivalent) database.
---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+include_once ('zip_lookup.php');
+
+// Define zip
+$zip = ($_GET['zip'] ? $_GET['zip'] : '');
+
+// Lookup city/state values
+$response = zip_lookup($zip);
+$city = $response['city'];
+$state = $response['state'];
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+<div class="container">
+    <h1>Lookup</h1>
+    <hr>
+    <form method="get">
+        <div class="form-group">
+            <label for="zip">Zip City/State</label>
+            <input id="zip" class="form-control" name="zip" type="number" minlength="3" value="<?php echo $zip; ?>">
+            <small id="zipHelp" class="form-text text-muted">Enter your zip code.</small>
+        </div>
+        <button type="submit" class="btn btn-primary">Get City and State</button>
+    </form>
+    <hr>
+    <h2>Response</h2>
+    <form method="get">
+        <div class="form-group">
+            <label for="zip">Zip Code</label>
+            <input id="zip" class="form-control" name="zip" value="<?php echo $zip; ?>" type="number" minlength="5" readonly>
+        </div>
+        <div class="form-group">
+            <label for="city">City</label>
+            <input id="cityHelp" class="form-control" name="city" value="<?php echo $city; ?>" type="text" readonly>
+        </div>
+        <div class="form-group">
+            <label for="state">State</label>
+            <input id="stateHelp" class="form-control" name="state" value="<?php echo $state; ?>" type="text" readonly>
+        </div>
+    </form>
+
+</div>
+</body>
+</html>
+
